099fd5a393488ee5d8076b178d04e225cc2050b2
Nurdiansyah <nur.diansyah.ckt@gmail.com>
2019-09-02 11:29:58 +0700

    fix: release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,11 +1,39 @@
 #!/usr/bin/env bash
 set -e
 
-export BABEL_ENV=production
+standardVersionOptions=
+while [[ "$1" != "" ]]; do
+  case $1 in
+  --dry-run | -d )
+    dryRun=1
+    standardVersionOptions="$standardVersionOptions --dry-run"
+    ;;
+  --prerelease | -p )
+    shift
+    preRelease=1
+    tagPublish="$1"
+    standardVersionOptions="$standardVersionOptions -p $1"
+    ;;
+  *)
+    standardVersionOptions="$standardVersionOptions $1"
+  esac
+  shift
+done
+
+
+export BABEL_ENV=$([[ -z ${preRelease}  ]] && echo "production" || echo "development" );
 rm -Rf libs;
 npm run build;
-standard-version "$@";
+standard-version ${standardVersionOptions};
 cp package.json libs
 cp -R types libs
 cd libs;
-npm publish;
+
+
+if [[ -z ${dryRun} ]]; then
+  if [[ -z ${tagPublish} ]]; then
+      npm publish
+    else
+      npm publish --tag ${tagPublish}
+  fi
+fi
